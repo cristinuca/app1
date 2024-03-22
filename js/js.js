c641df1acf16c23507f6e6f4cd2b77102857215b
Cristina <crishantig@yahoo.es>
2024-03-22 10:21:44 +0100

    muchos cambios

---
+++ js/js.js
@@ -0,0 +1,291 @@
+function uno(){
+let nota1,nota2,nota3;
+nota1=parseInt(prompt("ingrese la 1ª nota:"));
+nota2=parseInt(prompt("ingrese la 2ª nota:"));
+nota3=parseInt(prompt("ingrese la 3ª nota:"));
+resultado=(nota1+nota2+nota3)/3
+
+if(resultado>5){
+document.getElementById("demo").innerHTML="Aprobo la asignatura";
+
+
+}else{
+    document.getElementById("demo").innerHTML="No aprobó la asignatura";
+}
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function dos(){
+    let nota1,nota2,nota3;
+    nota1=parseInt(prompt("ingrese la 1ª nota:"));
+    nota2=parseInt(prompt("ingrese la 2ª nota:"));
+    nota3=parseInt(prompt("ingrese la 3ª nota:"));
+    resultado=(nota1+nota2+nota3)/3
+    
+    if(resultado>5){
+    document.getElementById("demo2").innerHTML="Aprobo la asignatura";
+    
+    
+    }else{
+        document.getElementById("demo2").innerHTML="No aprobó la asignatura";
+    }
+    
+    
+    
+    
+    
+    
+    }
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function tres(){
+        let nota1,nota2,nota3;
+        nota1=parseInt(prompt("ingrese la 1ª nota:"));
+        nota2=parseInt(prompt("ingrese la 2ª nota:"));
+        nota3=parseInt(prompt("ingrese la 3ª nota:"));
+        resultado=(nota1+nota2+nota3)/3
+        
+        if(resultado>5){
+        document.getElementById("demo3").innerHTML="Aprobo la asignatura";
+        
+        
+        }else{
+            document.getElementById("demo3").innerHTML="No aprobó la asignatura";
+        }
+        
+        
+        
+        
+        
+        
+        }
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+        function cuatro(){
+            let nota1,nota2,nota3;
+            nota1=parseInt(prompt("ingrese la 1ª nota:"));
+            nota2=parseInt(prompt("ingrese la 2ª nota:"));
+            nota3=parseInt(prompt("ingrese la 3ª nota:"));
+            resultado=(nota1+nota2+nota3)/3
+            
+            if(resultado>5){
+            document.getElementById("demo4").innerHTML="Aprobo la asignatura";
+            
+            
+            }else{
+                document.getElementById("demo4").innerHTML="No aprobó la asignatura";
+            }
+            
+            
+            
+            
+            
+            
+            }
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            function cinco(){
+                let nota1,nota2,nota3;
+                nota1=parseInt(prompt("ingrese la 1ª nota:"));
+                nota2=parseInt(prompt("ingrese la 2ª nota:"));
+                nota3=parseInt(prompt("ingrese la 3ª nota:"));
+                resultado=(nota1+nota2+nota3)/3
+                
+                if(resultado>5){
+                document.getElementById("demo5").innerHTML="Aprobo la asignatura";
+                
+                
+                }else{
+                    document.getElementById("demo5").innerHTML="No aprobó la asignatura";
+                }
+                
+                
+                
+                
+                
+                
+                }
+                
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+                function seis(){
+                    let nota1,nota2,nota3;
+                    nota1=parseInt(prompt("ingrese la 1ª nota:"));
+                    nota2=parseInt(prompt("ingrese la 2ª nota:"));
+                    nota3=parseInt(prompt("ingrese la 3ª nota:"));
+                    resultado=(nota1+nota2+nota3)/3
+                    
+                    if(resultado>5){
+                    document.getElementById("demo6").innerHTML="Aprobo la asignatura";
+                    
+                    
+                    }else{
+                        document.getElementById("demo6").innerHTML="No aprobó la asignatura";
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    }
+
+
+
+
+
+
+
+
+
+                    function nombre(){
+                        let nombre,apellido;
+                        nombre= prompt("coloque su nombre: ");
+                        apellido= prompt("coloque su nombre: ");
+                        document.getElementById("nombre").innerHTML= nombre + " " +apellido;
+                        
+                        
+                        
+                        
+                        }
+                    
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        function nombre1(){
+                            let nombre,apellido,direccion,codigo postal,telefono,email;
+                          
+                            document.getElementById("nombre1").innerHTML= nombre + " " +apellido;
+                            
+                            
+                            
+                            
+                            }
+
+
+
+
+//MATH.TRUNC   para redondear notas //
+
+
+
+
+
+
+
+
+
+
+
+
+function rellenarficha(){
+let nombre;
+nombre=(prompt("nombre: "));
+document.getElementById('dem1').innerHTML-nombre;
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+function borrar(){
+    document.getElementById("escribir").innerHTML=" ";
+}
+
+
---
+++ una cosa rara de internet/jsexp.js
@@ -0,0 +1,30 @@
+for(let i= 0; i<=10; i++){
+    console.log(i + " ");
+    document.write(i+ " ");
+    alert(i+" ");
+}
+
+
+
+
+🍬🍬🍬🍬🍪🍪🍪
+
+alert(felicidades)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
